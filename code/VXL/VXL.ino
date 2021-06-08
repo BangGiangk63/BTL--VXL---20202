77c8084b180d3b761d2ce0c9c718899bc27337b0
BangGiangk63 <69074592+BangGiangk63@users.noreply.github.com>
2021-06-08 21:47:23 +0700

    Update VXL.ino
    add code

--- code/VXL/VXL.ino
+++ code/VXL/VXL.ino
@@ -1,15 +1,17 @@
 #include "DHT.h"
-#include<Wifi.h>
 #include <Wire.h> 
+#include <BlynkSimpleEsp32.h>
 #include <LiquidCrystal_I2C.h>
+BlynkTimer timer;
 
-LiquidCrystal_I2C lcd(0x27,16,2);
-// khai báo ssid và mật khẩu để kết nối wifi:
-const char* SSID = "Minh Dien No7";
-const char*PASSWORD = "00001111";
+LiquidCrystal_I2C lcd(0x3F,16,2);
+const int DHTPIN = 8;
+const int DHTTYPE = DHT11;
+#define BLYNK_PRINT Serial
+char auth[] = "YourAuthToken";
+char SSID[] = "YourNetworkName";
+char PASSWORD[] = "YourPassword"
 // khai báo chân pin kết nối của dht11:
-#define DHTPIN2
-#define DHTTYPE DHT11
 DHT dht(DHTPIN,DHTTYPE);
 byte degree[8] = {
   0B01110,
@@ -25,6 +27,14 @@ void setup() {
   // put your setup code here, to run once:
   // khai báo baudrate:
   Serial.begin(115200);
+  //ket noi blynk:
+  Blynk.begin(auth, SSID, PASSWORD, "blynk.getblocky.com");
+  timer.setInterval
+  while (Blynk.connect() == false) {
+    // Wait until connected
+    delay(500);
+  }
+  Serial.println("Connected to Blynk server");
   //kết nối wifi :
   WiFi.begin(SSID,PASSWORD);
   Serial.print("\nConnecting to WiFi.");
@@ -34,11 +44,18 @@ void setup() {
   }
   Serial.println("\Connected to the WiFi network");
 }
+lcd.init();
+lcd.backlight();
+lcd.createChar(0, degree);
+lcd.setCursor(0,0);
+lcd.print("  DHT11   with ");
+lcd.setCursor(0,1);
+lcd.print("  ESP32 DevKit ");
+delay(3000);
+lcd.clear();
 // cho dht11 hoat dong:
 dht.begin();
-
   }
-
 void loop() {
   // put your main code here, to run repeatedly:
   // quy định cứ mỗi 5s esp sẽ đọc giá trị cảm biến và gửi về cho server:
@@ -49,13 +66,17 @@ void loop() {
   // kiểm tra xem giá trị đo được có đúng không:
   if (isnan(h) || isnan(t)) {
     Serial.println(F("Failed to read from DHT sensor!"));
-    return;
-  }
-  Serial.print(F("Temperature:"));
-  Serial.print(t);
-  Serial.print(F("°C "));
-  Serial.print(F("\tHumidity: "));
-  Serial.print(h);
-  Serial.print(F("% ");
+   }
+  else{
+  lcd.begin(16,2);
+  lcd.print("nhiet do: ");
+  lcd.print(t);
+  lcd.setCursor(0,1);
+  lcd.print("do am: ");
+  lcd.print(h);
+  lcd.setCursor(1,1);
+  delay(2000);
+  } 
 delay(5000);
+Blynk.run();
 }
